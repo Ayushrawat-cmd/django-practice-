63ca2265e0825188e4032deeb38b56374e837561
Ayush Rawat <ayushrawat@Ayushs-MacBook-Air.local>
2024-12-07 17:41:28 +0530

    Prodcut mixins view for each request

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_insert),
+    # path('', views.ProductListCreateAPIView.as_view()),
+    # path("<int:pk>/", views.ProductDetailAPIView.as_view()) # dynamic url 
     # path('/insert', views.api_insert)
+    path('', views.product_alt_view),
+    path('<int:pk>/', views.product_alt_view),
+    path('<int:pk>/update/', views.ProductUpdateView.as_view()),
+    path('<int:pk>/delete/', views.ProductDeleteView.as_view())
 ]   
--- backend/product/views.py
+++ backend/product/views.py
@@ -2,15 +2,94 @@ from django.shortcuts import render
 from django.forms.models import model_to_dict
 from django.http.request import HttpRequest
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
+
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer:serializer_class):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = title
+        serializer.save( content= content)
+
+        # send a django signal
+        # return super().perform_create(serializer)
+
+class ProductDetailAPIView(generics.RetrieveAPIView): # create rest api class
+    queryset = Product.objects.all()
+    # detail view basically help to look one single item
+    serializer_class =ProductSerializer
+    #lookupfield = 'pk'
+
+class ProductUpdateView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+    
+    def perform_update(self, serializer:ProductSerializer):
+        instance: Product = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            # instance.save()
+
+class ProductDeleteView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+
+    def perform_destroy(self, instance):
+        # instance oeration
+
+        super().perform_destroy(instance=instance)
+        return JsonResponse({"message":"Success fully deleted the product"})
+
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request:HttpRequest,pk= None, *args, **kwargs):
+    method = request.method
+    if method == "GET":
+        if pk is not None:
+            # Detail view
+            queryset = get_object_or_404 (Product, pk = pk)
+            data = ProductSerializer(queryset).data
+            return Response(data=data)
+
+        else:
+            qs= Product.objects.all()
+            data = ProductSerializer(qs, many =True).data
+            return Response(data=data)
+
+        # list view, get request- > detail view
+    elif method == "POST":
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+            serializer.save( content= content)
+            return Response(serializer.data)
+
+
 @api_view(["POST"])
 def api_insert(request: HttpRequest, *args, **kwargs):
     serializer = ProductSerializer(data = request.data)
-    if serializer.is_valid():
+    if serializer.is_valid(raise_exception=True):
         return JsonResponse(serializer.data)
 
 
